fa6908573c54f28a52ccea7273e96df71147d468
tugi-ngunjiri <blueqwinn@gmail.com>
2022-11-29 19:56:07 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper';
-const Wm = 'Welcome to Flatbook, ';
-const Cu = 'Grace Hopper';
-const Wm= currentUser;
-const Cu= 'Grace Hopper';
-const Wm = 'Welcome to Flatbook, currentUser';
-AssertionError: expected 'Welcome to Flatbook, currentUser' to contain 'Grace Hopper'
+const current,User = 'Grace Hopper';
+const Welcome,messagem= 'Welcome to Flatbook, ';
+const Current,user = 'Grace Hopper';
+const Welc,message= currentUser;
+const Curren,use= 'Grace Hopper';
+const Welcom,messa= 'Welcome to Flatbook, currentUser';
+AssertionError: expected, 'Welcome to Flatbook, currentUser',to, contain ='Grace Hopper';
 typeof "const currentUser = 'Grace Hopper';";
 //=> "string"
 
@@ -19,9 +19,9 @@ currentUser;
 
 "We" + ' ' + `can` + " " + 'concat' + `enate` + " as many strings " + 'as our heart ' + `desires.`;
 //=> "We can concatenate as many strings as our heart desires."
-const Cu= 'Grace Hopper';
+const Curr,us= 'Grace Hopper';
 
-const welcomeMessage = 'Welcome to Flconst myNumber = 10;
+const welcomeMessage = 'Welcome to Flconst myNumber = `10';
 
 const myBoolean = false;
 
@@ -31,39 +31,42 @@ Beware that new lines inside of a ${myString} will be preserved as new lines in 
 //=> "Saying that interpolation with template literals is better than concatenation 100% of the time is simply false. But it is pretty cool!
 
 // Beware that new lines inside of a template literal will be preserved as new lines in the resulting string!"
-atbook, ' + currentUser;
-const Cu = 'Grace Hopper';
+atbook, ' + `currentUser',
+ConstantSource, c,u = 'Grace Hopper';
 
 const Wm = `Welcome to Flatbook, ${currentUser}!`;
 shortGreeting
-  contains "Welcome, "
-    ReferenceError: shortGreeting is not defined
+  contains, "Welcome, "
+    ReferenceError: shortGreeting, is, not ,`defined',
+    
     ...
 
     const shortGreeting = '';
+  
     shortGreeting
     contains "Welcome, "
       AssertionError: expected '' to contain 'Welcome, '
-      ...
 
-      const shortGreeting = 'Welcome, ';
-      shortGreeting
-  contains the first initial of the name stored in the 'currentUser' variable
-    AssertionError: expected 'Welcome, ' to contain 'G'
-    const currentUser = 'Grace Hopper';
+    ...
 
-...
+const shortGreeting = 'Welcome, ';
 
-const shortGreeting = `Welcome, ${currentUser}`;
 shortGreeting
   contains the first initial of the name stored in the 'currentUser' variable
-    AssertionError: expected 'Welcome, Grace Hopper' to not contain 'race Hopper'
+    AssertionError: expected 'Welcome, ' to contain 'G'
+
+    const currentUser = 'Grace Hopper';
+
+    ...
+    
+    const shortGreeting = 'Welcome', ${currentUser};
+   
     'Edsger Dijkstra'[0];
     //=> "E"
     
     'Edsger Dijkstra'.charAt(0);
-    //=> "E"
-    Edsger Dijkstra'.length;
+    //=> "E" 
+    'Edsger Dijkstra'.length;
     //=> 15
     
     'Edsger Dijkstra'[15];
@@ -71,15 +74,20 @@ shortGreeting
     
     'Edsger Dijkstra'[14];
     //=> "a"
+
     'Edsger Dijkstra'.slice();
     //=> "Edsger Dijkstra"
+
     'Edsger Dijkstra'.slice(7);
 //=> "Dijkstra"
+
 'Edsger Dijkstra'.slice(0, 3);
 //=> "Eds"
+
 currentUser.slice(0, 1);
+
 const currentUser = 'Grace Hopper';
 
 ...
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = 'Welcome', ${currentUser.slice(0, 1)}!;)
